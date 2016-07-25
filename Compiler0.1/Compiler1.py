dde9c48897465a2cd46d3fed625f7d8711a14e5c
roboticsenthusiast <alanyuyi98@gmail.com>
2016-07-25 05:11:39 -0400

    Made some progress on lexer, added to thoughts

--- Compiler0.1/Compiler1.py
+++ Compiler0.1/Compiler1.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python2
+import re
 import argparse
 import string 
+#Define enum type (Enums aren't in python 2.7, only in python 3)
+def enum(**enums):
+    return type('Enum', (), enums)
+
 #Compiler input
 parser=argparse.ArgumentParser(description="Compile a target file into LaTeX.")
 parser.add_argument("InputFileName", metavar="File", default="E1", help="The path to the input file for this compiler")
@@ -21,7 +26,23 @@ for line in Alllines:
 TargetStringIntermediate="".join(OutputLines)		
 TargetString=TargetStringIntermediate.replace("	","/t")
 
-#Attempt to convert TargetString into an abstract syntax tree
+#Lexer section
 
+#Define tokens with regex
+#Review regex later for enhancements
 
+Tokens={'TAG':'(/def|/v|/eg|/!|//|=|==|===|;|/-)','TEXT':'(\w*\s*)+'}
+
+def lex(inputstr):
+    tokens=[]
+    tokenRegex=[]
+    for key in Tokens:
+        tokenRegex.append("|(?P<{groupName}>{actualRegex})".format(groupName=key,actualRegex=Tokens[key]))
+    tokenRegexstr=("".join(tokenRegex))[1:]
+    looker=re.compile(tokenRegexstr)
+    while (re.search(
+    print tokenRegexstr
+    return tokens
+
+new=lex()
 #target.close()
